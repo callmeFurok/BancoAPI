20cbec4c175044e7161e81437f9f6da81e7d6a56
Alfredo Aguirre <thefurok@gmail.com>
2023-04-30 18:33:37 -0500

    Modelo persona creado

---
+++ BancoAPI/Modelos/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BancoAPI.Modelos
+{
+    public class Persona
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Genero { get; set; }
+        [Required]
+        [MaxLength(3)]
+        public string Edad { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Identificacion { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Direccion { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Telefono { get; set; }
+    }
+}
