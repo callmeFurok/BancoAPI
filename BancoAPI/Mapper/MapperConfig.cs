0cf2e568c26d950da765dea87056ae3a7fed5ce3
Alfredo Aguirre <thefurok@gmail.com>
2023-05-01 18:59:26 -0500

    Configuraciones para usar AutoMapper

---
+++ BancoAPI/Mapper/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BancoAPI.Modelos;
+using BancoAPI.Modelos.Dtos;
+
+namespace BancoAPI.Mapper
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Cliente, CrearClienteDto>().ReverseMap();
+            CreateMap<Cuenta, CrearCuentaDto>().ReverseMap();
+        }
+    }
+}
