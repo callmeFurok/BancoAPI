9f3482be10e31750943e418ac2f6e5b12ef91d6d
Alfredo Aguirre <thefurok@gmail.com>
2023-04-30 20:20:58 -0500

    Carpeta de migraciones historicas de EF

---
+++ BancoAPI/Migrations/20230501011226_CreacionModelos.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BancoAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreacionModelos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Contrasenia = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Edad = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Identificacion = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cuentas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroCuenta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoCuenta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SaldoInicial = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cuentas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cuentas_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movimientos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TipoMovimiento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
+                    Saldo = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
+                    CuentaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimientos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movimientos_Cuentas_CuentaId",
+                        column: x => x.CuentaId,
+                        principalTable: "Cuentas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cuentas_ClienteId",
+                table: "Cuentas",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimientos_CuentaId",
+                table: "Movimientos",
+                column: "CuentaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movimientos");
+
+            migrationBuilder.DropTable(
+                name: "Cuentas");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
