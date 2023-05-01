698b0ce2723097f1086c1901f1f39fe32104f0ce
Alfredo Aguirre <thefurok@gmail.com>
2023-04-30 20:20:29 -0500

    Creada la clase de contexto para uso de EF con la base de datos

---
+++ BancoAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BancoAPI.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancoAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cuenta> Cuentas { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Movimientos> Movimientos { get; set; }
+    }
+}
