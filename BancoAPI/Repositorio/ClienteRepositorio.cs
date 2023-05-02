43b45d5bae587ca0081ba9e2ae8ff766be806b9b
Alfredo Aguirre <thefurok@gmail.com>
2023-05-01 20:50:45 -0500

    Repositorio generico agregado

--- BancoAPI/Repositorio/ClienteRepositorio.cs
+++ BancoAPI/Repositorio/ClienteRepositorio.cs
@@ -1,58 +1,17 @@
 ﻿using BancoAPI.Data;
 using BancoAPI.Modelos;
 using BancoAPI.Repositorio.IRepositorio;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace BancoAPI.Repositorio
 {
-    public class ClienteRepositorio : IClienteRepositorio
+    public class ClienteRepositorio : RepositorioGenerico<Cliente>, IClienteRepositorio
     {
         private readonly ApplicationDbContext _context;
 
-        public ClienteRepositorio(ApplicationDbContext context)
+        public ClienteRepositorio(ApplicationDbContext context) : base(context)
         {
             _context = context;
         }
-        public async Task CrearAsync(Cliente cliente)
-        {
-            await _context.Clientes.AddAsync(cliente);
-            await GuardarAsync();
-        }
-
-        public async Task EliminarAsync(Cliente cliente)
-        {
-            _context.Clientes.Remove(cliente);
-            await GuardarAsync();
-        }
-
-        public async Task GuardarAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<Cliente> ObtenerEspecificoAsync(Expression<Func<Cliente, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Cliente> query = _context.Clientes;
-            if (!tracked)
-                query = query.AsNoTracking();
-
-            if (filter != null)
-                query = query.Where(filter);
-
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Cliente>> ObtenerTodosAsync(Expression<Func<Cliente, bool>> filter = null)
-        {
-            IQueryable<Cliente> query = _context.Clientes;
-
-            if (filter != null)
-                query = query.Where(filter);
-
-            return await query.ToListAsync();
-        }
-
 
         public bool ExisteCliente(string nombre)
         {
@@ -63,7 +22,7 @@ namespace BancoAPI.Repositorio
         public async Task ActualizarAsync(Cliente cliente)
         {
             _context.Clientes.Update(cliente);
-            await GuardarAsync();
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ BancoAPI/Repositorio/RepositorioGenerico.cs
@@ -0,0 +1,59 @@
+﻿using BancoAPI.Data;
+using BancoAPI.Repositorio.IRepositorio;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BancoAPI.Repositorio
+{
+    public class RepositorioGenerico<T> : IRepositorioGenerico<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        internal DbSet<T> _dbSet;
+
+        public RepositorioGenerico(ApplicationDbContext context)
+        {
+            _context = context;
+            this._dbSet = _context.Set<T>();
+        }
+
+        public async Task CrearAsync(T entidad)
+        {
+            await _dbSet.AddAsync(entidad);
+            await GuardarAsync();
+        }
+
+        public async Task EliminarAsync(T entidad)
+        {
+            _dbSet.Remove(entidad);
+            await GuardarAsync();
+        }
+
+        public async Task GuardarAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> ObtenerEspecificoAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (!tracked)
+                query = query.AsNoTracking();
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> ObtenerTodosAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+                query = query.Where(filter);
+
+            return await query.ToListAsync();
+        }
+    }
+}
