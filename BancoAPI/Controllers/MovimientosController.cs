f53d73eb222e61d88335fefa93cea215a7d40b23
Alfredo Aguirre <thefurok@gmail.com>
2023-05-01 20:53:17 -0500

    Agregado el endpoint para movimientos

---
+++ BancoAPI/Controllers/MovimientosController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BancoAPI.Data;
+using BancoAPI.Modelos;
+
+namespace BancoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovimientosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovimientosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movimientos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movimientos>>> GetMovimientos()
+        {
+          if (_context.Movimientos == null)
+          {
+              return NotFound();
+          }
+            return await _context.Movimientos.ToListAsync();
+        }
+
+        // GET: api/Movimientos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movimientos>> GetMovimientos(Guid id)
+        {
+          if (_context.Movimientos == null)
+          {
+              return NotFound();
+          }
+            var movimientos = await _context.Movimientos.FindAsync(id);
+
+            if (movimientos == null)
+            {
+                return NotFound();
+            }
+
+            return movimientos;
+        }
+
+        // PUT: api/Movimientos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovimientos(Guid id, Movimientos movimientos)
+        {
+            if (id != movimientos.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movimientos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovimientosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Movimientos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Movimientos>> PostMovimientos(Movimientos movimientos)
+        {
+          if (_context.Movimientos == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Movimientos'  is null.");
+          }
+            _context.Movimientos.Add(movimientos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovimientos", new { id = movimientos.Id }, movimientos);
+        }
+
+        // DELETE: api/Movimientos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovimientos(Guid id)
+        {
+            if (_context.Movimientos == null)
+            {
+                return NotFound();
+            }
+            var movimientos = await _context.Movimientos.FindAsync(id);
+            if (movimientos == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movimientos.Remove(movimientos);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovimientosExists(Guid id)
+        {
+            return (_context.Movimientos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
