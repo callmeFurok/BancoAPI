6a3b232b2291d87585d1006cfd423857dc01db26
Alfredo Aguirre <thefurok@gmail.com>
2023-05-01 20:50:29 -0500

    Casi lloro al implementar genericos , no lo haga compa

--- BancoAPI/Repositorio/IRepositorio/IClienteRepositorio.cs
+++ BancoAPI/Repositorio/IRepositorio/IClienteRepositorio.cs
@@ -1,17 +1,12 @@
 ﻿using BancoAPI.Modelos;
-using System.Linq.Expressions;
 
 namespace BancoAPI.Repositorio.IRepositorio
 {
-    public interface IClienteRepositorio
+    public interface IClienteRepositorio : IRepositorioGenerico<Cliente>
     {
-        Task<List<Cliente>> ObtenerTodosAsync(Expression<Func<Cliente, bool>> filter = null);
-        Task<Cliente> ObtenerEspecificoAsync(Expression<Func<Cliente, bool>> filter = null, bool tracked = true);
 
-        Task CrearAsync(Cliente cliente);
         Task ActualizarAsync(Cliente cliente);
-        Task EliminarAsync(Cliente cliente);
-        Task GuardarAsync();
         bool ExisteCliente(string nombre);
     }
+
 }
---
+++ BancoAPI/Repositorio/IRepositorio/IRepositorioGenerico.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace BancoAPI.Repositorio.IRepositorio
+{
+    public interface IRepositorioGenerico<T> where T : class
+    {
+        Task<List<T>> ObtenerTodosAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> ObtenerEspecificoAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task CrearAsync(T cliente);
+        Task EliminarAsync(T cliente);
+        Task GuardarAsync();
+    }
+}
